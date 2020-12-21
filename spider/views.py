c64d0973615025f69bb7df527f548e5eb1819ed1
anwenzen <anwenzen@qq.com>
2020-12-21 22:40:07 +0800

    添加课设模块(部分)

--- spider/views.py
+++ spider/views.py
@@ -19,3 +19,12 @@ def spider_data(request):
         key_word = request.POST.get('key_word')
         Spider(key_word=key_word).main()
     return redirect('/spider/index?hint=done')
+
+
+def position_data_preview(request):
+    if request.method == "POST":
+        if 'key_word' in request.POST:
+            key_word = request.POST.get('key_word')
+            position_data = Position.objects.get()
+            return render(request, 'spider/position_preview.html', {'position_data': position_data})
+    return render(request, 'spider/position_preview.html', {'position_data': ''})

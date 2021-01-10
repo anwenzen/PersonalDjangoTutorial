0496b4ab9e0c17190af8d6cfdbe1aafcc5333977
anwenzen <anwenzen@qq.com>
2021-01-10 18:34:15 +0800

    继续搞

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog.models import Message
+from blog.models import Paper
+
+
+class MessageModelsAdmin(admin.ModelAdmin):
+    related_name = '留言'
+    list_display = ('name', 'message', 'date_time')
+    search_fields = ('name',)
+    ordering = ('date_time',)

1321f058e40b8507940f13ce269e59487a7419c9
anwenzen <anwenzen@qq.com>
2021-01-11 16:48:45 +0800

    继续搞

--- polls/urls.py
+++ polls/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('get_captcha/', views.get_captcha),
     path('logout/', views.logout),
     path('test', views.test),
+    path('excel/', views.export_teachers_excel),
+    path('pdf/', views.export_pdf),
+    path('teachers_data/', views.get_teachers_data),
+    path('data/', views.teacher_data),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,13 @@
+import io
+from io import BytesIO
+
+import xlwt
+from django.contrib.admin.utils import quote
 from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from reportlab.pdfgen import canvas
+
 from polls.models import Subject, Teacher, User
 from polls.utils import gen_random_code, Captcha, gen_md5_digest
 
@@ -85,3 +92,59 @@ def test(request):
     for i in request.POST:
         print(i, request.POST.get(i))
     return HttpResponse(request)
+
+
+def export_teachers_excel(request):
+    # 创建工作簿
+    wb = xlwt.Workbook()
+    # 添加工作表
+    sheet = wb.add_sheet('老师信息表')
+    # 查询所有老师的信息
+    queryset = Teacher.objects.all()
+    # 向Excel表单中写入表头
+    colnames = ('姓名', '介绍', '好评数', '差评数', '学科')
+    for index, name in enumerate(colnames):
+        sheet.write(0, index, name)
+    # 向单元格中写入老师的数据
+    props = ('name', 'detail', 'good_count', 'bad_count', 'subject')
+    for row, teacher in enumerate(queryset):
+        for col, prop in enumerate(props):
+            value = getattr(teacher, prop, '')
+            if isinstance(value, Subject):
+                value = value.name
+            sheet.write(row + 1, col, value)
+    # 保存Excel
+    buffer = BytesIO()
+    wb.save(buffer)
+    # 将二进制数据写入响应的消息体中并设置MIME类型
+    resp = HttpResponse(buffer.getvalue(), content_type='application/vnd.ms-excel')
+    # 中文文件名需要处理成百分号编码
+    filename = quote('老师.xls')
+    # 通过响应头告知浏览器下载该文件以及对应的文件名
+    resp['content-disposition'] = f'attachment; filename*=utf-8''{filename}'
+    return resp
+
+
+def export_pdf(request: HttpRequest) -> HttpResponse:
+    buffer = io.BytesIO()
+    pdf = canvas.Canvas(buffer)
+    pdf.setFont("Helvetica", 80)
+    pdf.setFillColorRGB(0.2, 0.5, 0.3)
+    pdf.drawString(100, 550, 'hello, world!')
+    pdf.showPage()
+    pdf.save()
+    resp = HttpResponse(buffer.getvalue(), content_type='application/pdf')
+    resp['content-disposition'] = 'inline; filename="demo.pdf"'
+    return resp
+
+
+def get_teachers_data(request):
+    queryset = Teacher.objects.all()
+    names = [teacher.name for teacher in queryset]
+    good_counts = [teacher.good_count for teacher in queryset]
+    bad_counts = [teacher.bad_count for teacher in queryset]
+    return JsonResponse({'names': names, 'good': good_counts, 'bad': bad_counts})
+
+
+def teacher_data(request):
+    return render(request, 'polls/data.html')
